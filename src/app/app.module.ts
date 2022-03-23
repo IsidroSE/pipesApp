59a03b734a71ae0a6be5af9f464e24f1458f45b9
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-03-23 12:38:23 +0100

    add page pipes basicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData( localeEs );
 
 @NgModule({
   declarations: [
@@ -13,9 +19,12 @@ import { AppRouterModule } from './app-router.module';
   imports: [
     BrowserModule,
     AppRouterModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
